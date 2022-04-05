b2252638eda3eadd8ca4e5012792de2b447dbe42
ninapgarcia <ninapgarcia@gmail.com>
2022-04-05 15:32:45 -0300

    blazepose code refactoration

--- scripts/blazepose.py
+++ scripts/blazepose.py
@@ -1,6 +1,8 @@
-# Cabeçalho ? 
-# Esse código é uma modificação da solução demonstração disponível em:
-# https://google.github.io/mediapipe/solutions/pose.html#python-solution-api
+"""Pose detection using BlazePose.
+
+Esse código é uma modificação da solução demonstração disponível em 
+https://google.github.io/mediapipe/solutions/pose.html#python-solution-api
+"""
 
 # General imports
 import sys
@@ -28,37 +30,68 @@ mp_pose = mp.solutions.pose
 # Set confidence predictions and tracking threshold
 THRESHOLD = 0.3
 
-def predictionToVideo(video_name, video_path, video_out_path, file_out_path,  profile):
+def predictionToVideo(video_name, video_path, 
+                      video_out_path, file_out_path,  profile):
+    """Predicts a pose for the person in the video
+
+    Predicts a pose for the person in the video (according to the selected
+    profile)
+    Displays in the screen the output video showing the selected joints and
+    their connections and angles
+    Displays a real time plot for the joint angles
+
+    Args:
+        video_name: a string with the name of the video to be used in the predicton
+        video_path: a string with the path to the video to be used in the prediction
+        video_out_path: a string with the path to the output prediction video
+        file_out_path: a string with the path to the output prediction file
+        profile: a string informing the profile os the person in the video
+            ('frontal', 'right', 'left')
+
+    Saves the output video (.avi) and two output files (.data and .pickle) with
+    a header and the selected keypoint locations for each frame
+    """
+
     # Get video
     video_capture = cv2.VideoCapture(video_path)
     if(not fileManagement.videoCheck(video_capture)):
         return
 
     # Create output video file
-    output_video = fileManagement.createOutputVideoFile(video_out_path, video_capture)
+    output_video = fileManagement.createOutputVideoFile(video_out_path,
+                                                        video_capture)
     video_capture.release()
 
     # Create output file data
-    file_metadata = fileManagement.setMetadata(video_name, poses.KEYPOINT_DICT_MOVENET, poses.JUMP_PROFILE_MOVENET[profile], video_path)
-    fileManagement.writeToJsonFile(file_out_path, file_metadata, write_mode='w+')
+    file_metadata = fileManagement.setMetadata(video_name, 
+                                               poses.KEYPOINT_DICT_MOVENET, 
+                                               poses.JUMP_PROFILE_MOVENET[profile], 
+                                               video_path)
+                                               
+    fileManagement.writeToJsonFile(file_out_path, 
+                                   file_metadata, 
+                                   write_mode='w+')
 
     # Initialize pickle output list
     pickle_output_list = []
 
     # Create video file to process
     video_capture = cv2.VideoCapture(video_path)
-    if(not fileManagement.videoCheck(video_capture)):
+    if not fileManagement.videoCheck(video_capture):
         return
 
     # define info for DataMonitor plotting
     channels = [
-        {'title': "Knee Angle", 'color': 'pink', 'y_label': 'Angle(deg)', 'x_label': "Time(s)", "width": 2},
-        {'title': "Feet Angle", 'color': 'cyan', 'y_label': 'Angle(deg)', 'x_label': "Time(s)", "width": 2}
+        {'title': "Knee Angle", 'color': 'pink', 
+        'y_label': 'Angle(deg)', 'x_label': "Time(s)", "width": 2},
+        {'title': "Feet Angle", 'color': 'cyan', 
+        'y_label': 'Angle(deg)', 'x_label': "Time(s)", "width": 2}
     ]
 
 
     # Apply blazepose network
-    with mp_pose.Pose(min_detection_confidence=THRESHOLD, min_tracking_confidence=THRESHOLD) as pose: 
+    with mp_pose.Pose(min_detection_confidence=THRESHOLD, 
+                      min_tracking_confidence=THRESHOLD) as pose: 
         # Pass by each frame of the video and draw points and connections
         with DataMonitor(channels=channels) as dm:
             startTime = time.time()
@@ -82,18 +115,34 @@ def predictionToVideo(video_name, video_path, video_out_path, file_out_path,  pr
                 pose_selected = poses.JUMP_PROFILE_BLAZEPOSE[profile]
 
                 # Get pairings of interest
-                keypoint_connections = poses.selectConnections(pose_selected, poses.KEYPOINT_DICT_BLAZEPOSE, mp_pose.POSE_CONNECTIONS, neural_network="blazepose")
-                selected_keypoints = poses.selectKeypoints(frame, mp_results_object.pose_landmarks, pose_selected, poses.KEYPOINT_DICT_BLAZEPOSE, 'blazepose')
+                keypoint_connections = poses.selectConnections(pose_selected, 
+                                                               poses.KEYPOINT_DICT_BLAZEPOSE, 
+                                                               mp_pose.POSE_CONNECTIONS, 
+                                                               neural_network="blazepose")
+
+                selected_keypoints = poses.selectKeypoints(frame, 
+                                                           mp_results_object.pose_landmarks, 
+                                                           pose_selected, 
+                                                           poses.KEYPOINT_DICT_BLAZEPOSE, 
+                                                           'blazepose')
 
                 # Draw joints and pairings
-                drawing.drawConnections(frame, selected_keypoints, keypoint_connections)
-                drawing.drawKeypoints(frame,selected_keypoints)
+                drawing.drawConnections(frame, 
+                                        selected_keypoints, 
+                                        keypoint_connections)
+                drawing.drawKeypoints(frame,
+                                      selected_keypoints)
 
                 # Get angles and draw it in the  screen
-                poses_angles = angles.getAngles(selected_keypoints, poses.ANGLES_BLAZEPOSE, profile, pose_selected)
+                poses_angles = angles.getAngles(selected_keypoints, 
+                                                poses.ANGLES_BLAZEPOSE, 
+                                                profile, 
+                                                pose_selected)
 
                 # Write angles data in the screen
-                drawing.writeAnglesLegends(frame, poses_angles, poses.ANGLES_BLAZEPOSE[profile])
+                drawing.writeAnglesLegends(frame, 
+                                           poses_angles, 
+                                           poses.ANGLES_BLAZEPOSE[profile])
 
                 # Update realtime angle graphics
                 dm.data = {
@@ -109,7 +158,9 @@ def predictionToVideo(video_name, video_path, video_out_path, file_out_path,  pr
 
                 # Write data to file
                 file_data = {'keypoints': selected_keypoints.tolist()}
-                fileManagement.writeToJsonFile(file_out_path, file_data, write_mode='a')
+                fileManagement.writeToJsonFile(file_out_path, 
+                                               file_data, 
+                                               write_mode='a')
                 
                 # Show image
                 cv2.namedWindow('Blazepose Lite', cv2.WINDOW_NORMAL)
@@ -133,4 +184,8 @@ if __name__ == "__main__":
     video_name, video_path, video_out_path, file_out_path, profile = userInterface.initialMenu("blazepose")
 
     # Make predictions
-    predictionToVideo(video_name, video_path, video_out_path, file_out_path, profile)
+    predictionToVideo(video_name, 
+                      video_path, 
+                      video_out_path, 
+                      file_out_path, 
+                      profile)
