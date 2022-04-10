2d94d05b2986656068ee885264f9df26d40585d7
Rafael-F-Barbosa <rafaelfbarbosa7@gmail.com>
2022-04-10 20:04:07 -0300

    changed data_monitor to styleguide

--- utils/data_monitor.py
+++ utils/data_monitor.py
@@ -1,28 +1,29 @@
-
+""" Defines DataMonitor class
+"""
 
 from multiprocessing import Process, Queue, TimeoutError
 from multiprocessing.connection import Connection
 from queue import Empty
 import time
-from turtle import width
-
-from click import style
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtGui, QtCore
 import numpy as np
 
 class DataMonitor(object):
-    """ Data Monitoring of externally manipulated data
-        The data-monitor runs QtCore.QTimer() in an extra multiprocessing.Process.
-        For a clean subprocess handling it is recommended to use DataMonitor in the with environment:
-        > with DataMonitor(channels=..., ...) as dm:
-        >     while True:
-        >         dm.data = <update data from external source>
-        >         <do something else>
+    """ DataMonitor class encapsulate threads logic to plot data in a non-block
+    way.
+
     """
 
     def __init__(self, data = None, channels = [{}], size_of_graph=1000):
-        """ Constructs a DataMonitor instance
+        """ Constructs a DataMonitor instance.
+        
+        Args: 
+            data:
+            channels: list with channels dictionaryies that should contain keys:
+            title, color, y_label, x_label, width and y_limit (optional)
+            size_of_graph: int that represents the size of graph
+        
         """
         # data handling
         self._data = data
@@ -33,16 +34,14 @@ class DataMonitor(object):
         index = 0
         for channel in channels:
             aux_channel = {
-                # **channel, 
                 'data': [0]*size_of_graph,
-                'index':index
+                'index':index,
             }
             for key in channel.keys():
                 aux_channel[key] = channel[key]
             self.channels.append(aux_channel)
             index+=1
         self.t = [0]*size_of_graph
-
 
         # Initialize timer
         self.start_time = time.time()
@@ -81,19 +80,28 @@ class DataMonitor(object):
             self._data_queue = None
 
     def start(self):
-        """ Starts the QtCore.QTimer() as subprocess (non-blocking, queue communication) """
+        """ Starts the QtCore.QTimer() as subprocess (non-blocking, 
+        queue communication) 
+        
+        """
         self._data_queue = Queue()
-        self._show_process = Process(name='animate', target=self.show, args=(self._data_queue, ))
+        self._show_process = Process(name='animate', 
+                                    target=self.show, 
+                                    args=(self._data_queue,))
         self._show_process.start()
 
     def stop(self):
-        """ Stop a potentially running QtCore.QTimer() as subprocess """
+        """ Stop a potentially running QtCore.QTimer() as subprocess 
+        
+        """
         if self._show_process is not None:
             self.__exit__(None, None, None)
 
     def show(self, data_queue: Connection):
-        """ Creates the QtCore.QTimer() and creates the plot (blocking) """
-        # 
+        """ Creates the QtCore.QTimer() and creates the plot (blocking) 
+        
+        """
+        # Set _data_queue and app atributes
         self._data_queue = data_queue
         self.app = QtGui.QApplication([])
 
@@ -114,16 +122,18 @@ class DataMonitor(object):
             channel['plot'].setLabel('left', channel['y_label'])
             channel['plot'].setLabel('bottom', channel['x_label'])
             if('y_limit' in channel):
-                channel['plot'].setYRange(channel['y_limit'][0], channel['y_limit'][1], padding=0)
+                channel['plot'].setYRange(channel['y_limit'][0], 
+                                         channel['y_limit'][1], 
+                                         padding=0)
             
-
-
-            pen = pg.mkPen(channel['color'], width=channel['width'], style=QtCore.Qt.SolidLine)
+            pen = pg.mkPen(channel['color'], 
+                          width=channel['width'], 
+                          style=QtCore.Qt.SolidLine)
             channel['curve'] = channel['plot'].plot(pen = pen)
 
         graphUpdateSpeedMs = 1
-        timer = QtCore.QTimer()#to create a thread that calls a function at intervals
-        timer.timeout.connect(self.animate)#the update function keeps getting called at intervals
+        timer = QtCore.QTimer()
+        timer.timeout.connect(self.animate)
         timer.start(graphUpdateSpeedMs)   
         QtGui.QApplication.instance().exec_()
 
@@ -149,13 +159,16 @@ class DataMonitor(object):
     @data.setter
     def data(self, value):
         """ Puts data to the multiprocessing data queue
-            which is then received by the function animation.
+            which is then received by the qt plot animation.
+        
         """
 
         self._data_queue.put(value)
 
     def animate(self):
-        """ The update method of the matplotlib function animation """
+        """ The update method of qt plot animation 
+        
+        """
         data = self.data
         if data is not None:
             self.data_vector.append(data)
@@ -163,7 +176,6 @@ class DataMonitor(object):
         if data is None:
             return
 
-        print("Data update", data)
         # Set data for each plot
         for channel in self.channels:
             channel['data'][0:-1] = channel['data'][1:]
@@ -171,7 +183,6 @@ class DataMonitor(object):
 
         self.t[0:-1] = self.t[1:]
         self.t[-1] = data['time']
-
 
         for channel in self.channels:
             channel['curve'].setData(self.t, channel['data'])
@@ -183,9 +194,27 @@ class DataMonitor(object):
 if __name__ == '__main__':
     # Define amount of channels and it's caracteristics
     channels = [
-        {'title': "Angle 1", 'color': 'cyan', 'y_label': 'Angle(deg)', 'x_label': "Time(s)", "width": 2}, 
-        {'title': 'Angle2', 'color': 'pink','y_label': 'Angle(deg)', 'x_label': "Time(s)", "width": 2},
-        {'title': 'Angle3', 'color': 'blue','y_label': 'Angle(deg)', 'x_label': "Time(s)", "width": 4}
+        {
+            'title': "Angle 1", 
+            'color': 'cyan', 
+            'y_label': 'Angle(deg)', 
+            'x_label': "Time(s)", 
+            "width": 2,
+        }, 
+        {
+            'title': 'Angle2', 
+            'color': 'pink',
+            'y_label': 'Angle(deg)', 
+            'x_label': "Time(s)", 
+            "width": 2
+        },
+        {
+            'title': 'Angle3', 
+            'color': 'blue',
+            'y_label': 'Angle(deg)', 
+            'x_label': "Time(s)", 
+            "width": 4
+        }
     ]
     with DataMonitor(channels=channels) as dm:
         startTime = time.time()
