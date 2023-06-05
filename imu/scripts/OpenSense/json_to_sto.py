97b194b61f116366f26b29cc6c8d9fd6900cbe96
ninapgarcia <ninapgarcia@gmail.com>
2023-06-05 12:33:24 -0300

    Change data format to mach .sto

--- imu/scripts/OpenSense/json_to_sto.py
+++ imu/scripts/OpenSense/json_to_sto.py
@@ -1,54 +1,39 @@
 import pandas as pd
+import numpy as np
 import json
 
 
+# Read JSON file to Data Frame 
+df = pd.read_json('data/teste.json', lines=True)
 
-df = pd.read_json('teste.json', lines=True)
-
+# Drop rows before 0.1s
 df = df.drop(df[df['time_stamp'] < 0.1].index)
-
 df = df.reset_index(drop=True)
 
-print(df.to_string())
 
-#---------------------------------------------------------
+# Change data format to mach .sto
+df['quaternion_ankle'] = df['quaternion_ankle'].str.replace('[','', regex=True)
+df['quaternion_ankle'] = df['quaternion_ankle'].str.replace(']','', regex=True)
+df['quaternion_ankle'] = df['quaternion_ankle'].str.replace('   ',',', regex=True)
+df['quaternion_ankle'] = df['quaternion_ankle'].str.replace('  ',',', regex=True)
+df['quaternion_ankle'] = df['quaternion_ankle'].str.lstrip()
+df['quaternion_ankle'] = df['quaternion_ankle'].str.replace('/^\w+/\s', '', regex=True)
+
+df['quaternion_thigh'] = df['quaternion_thigh'].str.replace('[','', regex=True)
+df['quaternion_thigh'] = df['quaternion_thigh'].str.replace(']','', regex=True)
+df['quaternion_thigh'] = df['quaternion_thigh'].str.replace('   ',',', regex=True)
+df['quaternion_thigh'] = df['quaternion_thigh'].str.replace('  ',',', regex=True)
+df['quaternion_thigh'] = df['quaternion_thigh'].str.lstrip()
+df['quaternion_thigh'] = df['quaternion_thigh'].str.replace(' ',',', regex=True)
 
 
-#file = open(stateFile, 'r')
+# Rename columns
+df = df.rename(columns={'time_stamp':'time', 'quaternion_ankle': 'tibia_r_imu', 'quaternion_thigh': 'femur_r_imu'})
+
+
+print(df)
+
+# Save file .sto
 nomeOut = 'teste.sto'
-#fileOut = open(nomeOut, 'w')
-
-# linhaHeader = 0
-# for linha in file:
-#     if linha.find("endheader") >= 0:
-#         break
-#     else:
-#         linhaHeader = linhaHeader + 1
-#         fileOut.write(linha)
-
-# fileOut.write("endheader\n")
-# fileOut.close()
-# file.close()
-
-#dados = pd.read_table(stateFile, header=linhaHeader + 1)
-
-#dadosOut = dados
-
-colunas = ['time',
-            'quaternions_thigh',
-            'quaternions_ankle']
-
-# Remover colunas que não estão na lista
-# for col in df.columns:
-#     if not(col in colunas):
-#         del dadosOut[col]
-
-# Deixando os valores simetricos lateralmente
-# dadosOut['/jointset/hip_l/hip_flexion_l/value'] = dadosOut['/jointset/hip_r/hip_flexion_r/value']
-# dadosOut['/jointset/knee_l/knee_angle_l/value'] = dadosOut['/jointset/knee_r/knee_angle_r/value']
-# dadosOut['/jointset/ankle_l/ankle_angle_l/value'] = dadosOut['/jointset/ankle_r/ankle_angle_r/value']
-
-#dadosOut['/jointset/back/lumbar_extension/value'] = np.zeros(len(dadosOut['time']))
-
 df.to_csv(nomeOut, header=True, index=None, sep='\t', mode='a')
 
